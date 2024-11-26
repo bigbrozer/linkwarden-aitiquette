000ac90c0367118dda86d868474f0db07b618718
BESANCON Vincent <vincent.besancon@forvia.com>
2024-11-26 14:43:06 +0100

    feat(prompts): add translation

--- src/prompts.rs
+++ src/prompts.rs
@@ -6,7 +6,7 @@ pub fn build_summary() -> ChatCompletionMessage {
     ChatCompletionMessage {
         role: MessageRole::system,
         content: Content::Text(
-            "You receive complete article content as input. You must condense it to 1-3 sentences maximum. Only return the answer."
+            "You receive complete article content as input. Translate all content to french and you must describe it to 1-3 sentences maximum. Only return the translated content without explanations or introduction."
                 .to_string(),
         ),
         name: None,
@@ -21,12 +21,10 @@ pub fn build_tagging() -> ChatCompletionMessage {
         content: Content::Text(r#"
 You are a bot in a read-it-later app and your responsibility is to help with automatic tagging.
 Please analyze the text and suggest relevant tags that describe its key themes, topics, and main ideas. Follow strictly the following rules:
-- Aim for a variety of tags, including broad categories, specific keywords, and potential sub-genres.
-- Write tags in english.
+- Aim for a variety of tags, including broad categories, specific keywords, popular nouns, brands and potential sub-genres.
+- Write tags in french.
 - Use lower case.
 - Try to restrict tag to one word.
-- If it is a brand or a well-known topic you may also include a tag for it.
-- If it's a famous website you may also include a tag for the website. If the tag is not generic enough, don't include it.
 - The content can include text for cookie consent and privacy policy, ignore those while tagging.
 - Aim for 3-5 short and concise tags.
 - If there are no good tags, leave the array empty.
